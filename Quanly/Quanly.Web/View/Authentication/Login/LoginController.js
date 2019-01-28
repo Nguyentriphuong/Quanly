681146dfb4a2af403ef148dd7d92bb60fff1035c
Nguyentriphuong <Nguyentriphuong2107@gmail.com>
2019-01-28 16:34:05 +0700

    updateView

---
+++ Quanly/Quanly.Web/View/Authentication/Login/LoginController.js
@@ -0,0 +1,7 @@
+﻿myApp.controller('loginController', function ($scope, $http) {
+    $scope.load = $scope.userName;
+    $scope.login = function () {
+        $scope.load = $scope.userName;
+    }
+
+});
