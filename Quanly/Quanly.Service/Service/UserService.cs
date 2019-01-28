6b99da72b137234875cc09f07c6049a8ed66b782
Nguyentriphuong <Nguyentriphuong2107@gmail.com>
2019-01-29 03:19:59 +0700

    us

---
+++ Quanly/Quanly.Service/Service/UserService.cs
@@ -0,0 +1,21 @@
+﻿using Quanly.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Quanly.Service.Service
+{
+    
+    public class UserService
+    {
+        private QuanlyEntities db = new QuanlyEntities();
+        public User GetUser(string Username, string Password)
+        {
+
+            var user = db.Users.Where(x => x.Username.Equals(Username) && x.Password.Equals(Password)).FirstOrDefault();
+            return user;
+                 
+        }
+    }
+}
